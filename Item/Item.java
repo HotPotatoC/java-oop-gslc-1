0ac34ed49cc7136f88a347e505f20443664218bd
HotPotatoC <juandotulung@gmail.com>
2022-11-08 01:07:12 +0700

    fix: quantity price

--- Item/Item.java
+++ Item/Item.java
@@ -7,7 +7,7 @@ public class Item {
 
     public Item(String name, double price, int quantity) {
         this.name = name;
-        this.price = price;
+        this.price = price * quantity;
         this.quantity = quantity;
     }
 
